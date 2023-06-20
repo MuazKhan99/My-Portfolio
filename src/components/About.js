59b11cbe9587ad9a699ea8f5ed9e9c30869f9ae0
MuazKhan99 <muazkhan99@gmail.com>
2023-06-20 15:23:23 +0200

    Finalized Portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -17,9 +17,11 @@ const About = () => {
           transition={{ duration: 0.5 }}
         >
           <h2>About Me</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed purus tincidunt, congue elit non, suscipit sem.</p>
-          <p>Quisque ultricies, sapien nec vestibulum commodo, lectus odio scelerisque dui, at bibendum nunc dolor sed urna. Nullam at ipsum ut nunc mollis feugiat.</p>
-          <p>Pellentesque in leo eget mauris facilisis sagittis non id quam. Donec egestas varius suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
+          <p>I recently graduated from the University of Kristianstad, where I obtained a Bachelor's degree in Software Development. As a goal-oriented individual, I bring forth a strong dedication, passion, and knowledge to every task assigned to me. My unwavering commitment to success and my drive to excel in my field motivate me to continuously enhance my skills and knowledge.</p>
+          <p>During my academic journey, I had the opportunity to undertake a significant project with Volvo Cars, focusing on the application of graph-based theory to detect error patterns in software updates for cars. This experience allowed me to gain valuable insights into the practical aspects of software development and the importance of meticulous error analysis. Additionally, I gained hands-on experience as an Android Developer at Softhouse, where I contributed to the development of an event planning app. Furthermore, I honed my skills as a Development Intern at Revam AB, where I actively participated in modifying website designs and organizing databases.</p>
+          <p>My technical skill set encompasses a wide range of technologies, including proficiency in Java, JavaScript, Python, and Kotlin. I am well-versed in mobile app development, utilizing frameworks such as Flask and implementing web technologies such as HTML, CSS, and React with Material UI. Moreover, I have hands-on experience with Node.js, MySQL, SQLAlchemy, SQLite, MongoDB, and Neo4j databases. Additionally, I possess knowledge in areas such as Wireshark, Scrum, and CyberSecurity, which further enhances my ability to tackle complex challenges.</p>
+          <p>In addition to my technical expertise, I am proficient in English and have a limited working proficiency in Swedish. This enables effective communication and collaboration within international teams and allows me to adapt to diverse work environments.</p>
+          <p>I am constantly driven to learn and grow, seeking new opportunities to apply my skills and contribute to innovative projects. With a strong foundation in software development and a passion for excellence, I am eager to embark on new challenges and make a positive impact in the field.</p>
         </motion.div>
       </div>
     </section>
