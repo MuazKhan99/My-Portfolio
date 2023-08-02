bdd588170ef1811609209fb6dda8d4e12a139be7
MuazKhan99 <muazkhan99@gmail.com>
2023-08-02 22:21:50 +0200

    Minor fixes and changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import FadeInOut from './FadeInOut';
 
-const Header = () => {
-  // Function to handle the resume download
-  const handleDownloadResume = () => {
-    const resumeFile = "/Muhammad_Muaz_Khan's_Resume.pdf"; // Adjust the path to your actual resume file
-    window.open(resumeFile, '_blank');
-  };
+const handleDownloadResume = () => {
+  const link = document.createElement('a');
+  link.href = "https://muazkhan99.github.io/My-Portfolio/Muhammad_Muaz_Khan's_Resume.pdf";
+  link.download = "Muhammad_Muaz_Khan's_Resume.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
+const Header = () => {
   return (
     <header>
       <nav>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,19 +11,16 @@ const Projects = () => {
             <h3>Volvo Cars Thesis Project</h3>
             <p>During my studies at the University of Kristianstad, I completed a Bachelor's thesis project with Volvo Cars. The project focused on applying graph-based theory to detect error patterns in software updates for cars. I built a Neo4j graph database using their data and utilized advanced analysis techniques to identify and analyze errors.</p>
             <p>Technologies used: Neo4j, Python, Cypher</p>
-            <a href="https://github.com/MuazKhan99/Thesis_Neo4j_Implementation">View on GitHub</a>
           </div>
           <div className="project">
             <h3>Event Planning App</h3>
             <p>As an Android Developer at Softhouse, I collaborated with a group to develop an event planning app. I played a crucial role in implementing key features, including setting up a sign-in button, integrating a Firebase database, and ensuring user-friendly functionality.</p>
             <p>Technologies used: Kotlin, Android Studio, Firebase</p>
-            <a href="https://github.com/MuazKhan99/EventHouse">View on GitHub</a>
           </div>
           <div className="project">
             <h3>Revam AB Website Modification</h3>
             <p>During my internship at Revam AB, I contributed to the modification of their website design. I was responsible for creating a webpage that incorporated a feedback form, enhancing user engagement and interaction. Additionally, I assisted in organizing the backend database to optimize data management.</p>
             <p>Technologies used: HTML, CSS, JavaScript</p>
-            <a href="https://revam.tech/">Visit Website</a>
           </div>
           <div className="project">
             <h3>IKEA Student Day Event</h3>
